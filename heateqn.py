812df291dc89b09cd0d97361cad77ffe96329c97
ehurturk <emirhurturk444@gmail.com>
2023-04-02 14:21:17 +0300

    use always_redraw

--- heateqn.py
+++ heateqn.py
@@ -15,8 +15,8 @@ class Simulation(Scene):
         return res
     
     def construct(self):
-        x_min, x_max, x_step = 0, 7, 1
-        y_min, y_max, y_step = -1, 3, 1
+        x_min, x_max, x_step = 0, int(np.ceil(2*np.pi)), 1
+        y_min, y_max, y_step = -1, 2, 1
         axes = Axes(
             x_range=[x_min, x_max + x_step, x_step],
             y_range=[y_min, y_max + y_step, y_step],
@@ -29,38 +29,36 @@ class Simulation(Scene):
         )
         x_label = axes.get_x_axis_label("l")
         y_label = axes.get_y_axis_label("T")
-        title = MathTex(r"\text{Heat Equation (n=5)}").next_to(axes, UP*1.5)
+        title = MathTex(r"\text{Heat Equation (n=10)}").next_to(axes, UP*1.5)
              
-        k = lambda x: 2.7-0.3*((x-3)**2)
-        a = 0.5 # gold (m2/s)
-        l = 6
+        # k = lambda x: 2.7-0.3*((x-3)**2)
+        k = lambda x: np.sin(x)
+        a = 0.0127 # gold (m2/s)
+        # l = 6
+        l = 2 * np.pi
         
-        t = ValueTracker(0)
+        t = ValueTracker(0.0)
         decimal = DecimalNumber(0.0)
+        
         x_vals = np.linspace(0.0, float(l), num=30)
-
-        def update_graph(mob):
-            mob.become(
-                axes.plot_line_graph(
-                    x_vals, 
-                    [self.heat(x, t.get_value(), l, a, k, num_runs=10) for x in x_vals],
-                    vertex_dot_radius=0
-                )
-            )
-
-        graph = VMobject()
-        graph.add_updater(update_graph)
+        graph = always_redraw(lambda: axes.plot_line_graph(
+            x_vals,
+            [self.heat(x, t.get_value(), l, a, k, num_runs=10)
+             for x in x_vals],
+            vertex_dot_radius=0
+        ))
         
         formula = MathTex("t = ", "s").to_corner(UR)
         formula[0].shift(LEFT * 1.1)
         decimal.next_to(formula[1], LEFT, aligned_edge=DOWN)
-        alpha = MathTex("\\alpha = 0.5").next_to(formula, DOWN)
-        obj_length = MathTex("L = 6 m").next_to(alpha, DOWN)
+        alpha = MathTex("\\alpha = "+str(a)).next_to(formula, DOWN)
+        obj_length = MathTex("l = 2\\pi \\ m").next_to(alpha, DOWN)
+        initial_func = MathTex("k(x) = \\sin(x)").next_to(obj_length, DOWN)
         
         self.play(Create(axes), Write(x_label), Write(y_label), Write(
-            title), Write(VGroup(formula[0], decimal, formula[1])), Write(alpha), Write(obj_length))
-        self.play(Create(graph))
-        self.wait()
-        for i in range(1, 6):
-            self.play(t.animate.set_value(i+1),ChangeDecimalToValue(decimal, i))
+            title), Write(VGroup(formula[0], decimal, formula[1])), Write(alpha), Write(obj_length), Write(initial_func))
+        self.play(Write(graph))
+        
+        self.play(t.animate(run_time=6).set_value(t.get_value()+50),
+                  ChangeDecimalToValue(decimal, t.get_value()+50), run_time=6)
         self.wait()
